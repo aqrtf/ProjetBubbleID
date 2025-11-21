870618a7321ea6af311c05cf2dbbefb8f6143a53
Armand <armand.farabolini@epfl.ch>
2025-11-21 08:29:18 +0100

    ajout d'autres rtest

--- Customizable/test2.py
+++ Customizable/test2.py
@@ -1,5 +1,17 @@
-import numpy as np
+import pandas as pd
 
-data_charge = np.load(r"C:\Users\faraboli\Desktop\BubbleID\BubbleIDGit\ProjetBubbleID\My_output\Test6\frames_Test6.npy", allow_pickle=True)
-print(data_charge)
-print(len(data_charge))
+df = pd.DataFrame({
+    'A': [1, 'x', 3, 'x', 5],
+    'B': ['x', 2, 'x', 4, 5],
+    'C': [10, 20, 30, 40, 50]
+})
+
+# Remplacer seulement dans certaines colonnes
+df_specific = df.replace({'A': {'x': 'remplacé_A'}, 'B': {'x': 'remplacé_B'}})
+print("Remplacement par colonne:")
+print(df_specific)
+
+# Utiliser where pour des conditions complexes
+df_where = df.where(df != 'x', 'conditionnel')
+print("\nAvec where:")
+print(df_where)
