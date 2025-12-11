ddc1742084ab18dae3988c5cad8299c29cfcb3ac
Armand <armand.farabolini@epfl.ch>
2025-12-11 17:13:27 +0100

    on prend la vitesse entre les merges

--- Customizable/BubbleID_dependencies/velocities.py
+++ Customizable/BubbleID_dependencies/velocities.py
@@ -1,6 +1,10 @@
 import os, math, csv, ast, json
 import numpy as np, pandas as pd
 from csteDef import *
+from computeDiameter import bubbleDiameter
+
+# TODO pour la velocity il faut prendre en compte que entre les merges
+# TODO il serait bien de comparer la vitesse avec le diameter
 
 # Classe pour stocker les vitesses et statistiques
 class velocities:
@@ -8,19 +12,30 @@ class velocities:
         self.vy = []
         self.dx = []
         self.sizeBlock = []
+        self.diameter = []
         self.vMean = -1
-        self.vMeanPerBubble = None
+        self.vMeanPerBlock = None
         self.vStd = -1
-        self.vStdPerBubble = None
-
+        self.vStdPerBlock = None
+        
+    def computeMean(self):
+        """Calcule la vitesse moyenne globale et par bulle."""
+        self.vMeanPerBlock = [arr.mean() for arr in self.vy]
+        self.vMean = np.mean(np.concatenate(self.vy))
+        self.vStdPerBlock = [arr.std() for arr in self.vy]
+        self.vStd = np.std(np.concatenate(self.vy))
+        self.diameterMeanPerBlock = [arr.mean() for arr in self.diameter]
+        self.diameterStdPerBlock = [arr.std() for arr in self.diameter]
+        
     def convert2mm(self, mm_per_px):
         """Convertit toutes les vitesses et déplacements en millimètres."""
         self.vy_mm = [x * mm_per_px for x in self.vy]
         self.dx_mm = [x * mm_per_px for x in self.dx]
         self.vMean_mm = self.vMean * mm_per_px
-        self.vMeanPerBubble_mm = np.array(self.vMeanPerBubble) * mm_per_px
+        self.vMeanPerBubble_mm = np.array(self.vMeanPerBlock) * mm_per_px
         self.vStd_mm = self.vStd * mm_per_px
-        self.vStdPerBubble_mm = np.array(self.vStdPerBubble) * mm_per_px
+        self.vStdPerBubble_mm = np.array(self.vStdPerBlock) * mm_per_px
+        self.diameter_mm = [x * mm_per_px for x in self.diameter]
 
 
 def extractPosition(frame0, tid, contours, rich_df, position):
@@ -50,19 +65,37 @@ def extractPosition(frame0, tid, contours, rich_df, position):
     return coord
 
 
-def compute_speed_blocks(frames0, track_ids, labels, contours, rich_df,
+def compute_speed_blocks(frames0, track_ids, labels, mergeFrames, contours, rich_df,
                          attach_vel, detach_vel, minPointForVelocity, fps):
     """
     Calcule les vitesses par bloc de labels (ATTACHED / DETACHED).
     Remplit les objets attach_vel et detach_vel.
     """
+    
+
+        
     df = pd.DataFrame({
         "frame": frames0,
         "track_id": track_ids,
         "label": labels
     })
+    
+    df["merge"]=0
+    for i in mergeFrames: 
+        df["merge"] = df["merge"] + (df["frame"]>i).astype(int) # TODO verifier a partir de quelle frame prendre i, i-1, i-2 ???
+    
+    
     df["time"] = df["frame"] / fps
-    df["block"] = (df["label"] != df["label"].shift()).cumsum()
+    df["block"] = (df["label"] != df["label"].shift()).cumsum() # block par label
+    df["block"] = (df["block"]) + (df["merge"] != df["merge"].shift()).cumsum() # block par merge et par label
+
+    for i in mergeFrames: 
+        # NOTE on retire la frame ou a lieu le merge de l'analyse, est ce utile ??
+        # Supprimer toutes les lignes où col1 est dans la liste
+        # df = df[~df["col1"].isin(to_remove)]
+        df = df[df["frame"] != i] # TODO verifier a partir de quelle frame prendre i, i-1, i-2 ???
+
+
 
     for _, group in df.groupby("block"):
         label = group["label"].iloc[0]
@@ -77,6 +110,9 @@ def compute_speed_blocks(frames0, track_ids, labels, contours, rich_df,
                   for fr, tid in zip(group["frame"], group["track_id"])]
         if len(coords) < minPointForVelocity:
             continue
+        
+        diameters = np.array([bubbleDiameter(fr, tid, rich_df)
+                     for fr, tid in zip(group["frame"], group["track_id"])])
 
         x = [c[0] for c in coords]
         y = [c[1] for c in coords]
@@ -86,6 +122,8 @@ def compute_speed_blocks(frames0, track_ids, labels, contours, rich_df,
         dy = np.diff(y)
         dt = np.diff(t)
         vy = - dy / dt  # origine en haut à gauche
+        
+        # NOTE normalement on ne devrait plus avoir de vitesse negative puisqu'on ne prend plus ce qui se passe au merge
 
         # On retire les vitesse negative
         dypos = dy[dy<0]
@@ -98,14 +136,17 @@ def compute_speed_blocks(frames0, track_ids, labels, contours, rich_df,
         
         vypos = - dypos[1:]/dtpos
 
+        # TODO ajouter le diametre
         if label == ATTACHED:
             attach_vel.vy.append(vypos)
             attach_vel.dx.append(dxpos)
             attach_vel.sizeBlock.append(vypos.size)
+            attach_vel.diameter.append(diameters) # NOTE diameter a un element de plus que vy
         else:
             detach_vel.vy.append(vypos)
             detach_vel.dx.append(dxpos)
             detach_vel.sizeBlock.append(vypos.size)
+            detach_vel.diameter.append(diameters)
 
 
 def bubble_velocities(savefolder, extension, minPointForVelocity=2, fps=4000):
@@ -158,6 +199,8 @@ def bubble_velocities(savefolder, extension, minPointForVelocity=2, fps=4000):
                                  (rich_df["track_id"] == tid_arr[irow][fr])].iloc[0].at["class_id"]))
         bubclass_arr.append(x)
 
+    mergeFrame_arr = df_evol["mergeFrame"].apply(ast.literal_eval).to_list()
+
     # Création des objets velocities
     attach_vel = velocities()
     detach_vel = velocities()
@@ -166,23 +209,17 @@ def bubble_velocities(savefolder, extension, minPointForVelocity=2, fps=4000):
     for idx, tid_evol in enumerate(tid_arr):
         frames0 = frames_arr[idx]
         labels = bubclass_arr[idx]
+        mergeFrames = mergeFrame_arr[idx]
         track_ids = [x for x in tid_evol if x is not None]
         # TODO smooth labels
-        compute_speed_blocks(frames0, track_ids, labels, contours, rich_df,
+        compute_speed_blocks(frames0, track_ids, labels, mergeFrames, contours, rich_df,
                              attach_vel, detach_vel, minPointForVelocity, fps)
 
     # TODO ajouter une condition si dx trop grand
 
     # Calcul des statistiques globales
-    detach_vel.vMeanPerBubble = [arr.mean() for arr in detach_vel.vy]
-    detach_vel.vMean = np.mean(np.concatenate(detach_vel.vy))
-    detach_vel.vStdPerBubble = [arr.std() for arr in detach_vel.vy]
-    detach_vel.vStd = np.std(np.concatenate(detach_vel.vy))
-
-    attach_vel.vMeanPerBubble = [arr.mean() for arr in attach_vel.vy]
-    attach_vel.vMean = np.mean(np.concatenate(attach_vel.vy))
-    attach_vel.vStdPerBubble = [arr.std() for arr in attach_vel.vy]
-    attach_vel.vStd = np.std(np.concatenate(attach_vel.vy))
+    detach_vel.computeMean()
+    attach_vel.computeMean()
 
     attach_vel.convert2mm(mm_per_px)
     detach_vel.convert2mm(mm_per_px)
